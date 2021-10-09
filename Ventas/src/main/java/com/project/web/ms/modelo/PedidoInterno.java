20dc93c2b1a24abc8722c10a54e1878976047a58
Miguel-Karley <92017400+Miguel-Karley@users.noreply.github.com>
2021-10-09 17:37:26 -0500

    Add files via upload

---
+++ Ventas/src/main/java/com/project/web/ms/modelo/PedidoInterno.java
@@ -0,0 +1,40 @@
+package com.project.web.ms.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name = "pedidointerno")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class PedidoInterno {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long idpedido;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "idproducto")
+	@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+	private Producto producto;
+	
+	private Integer cantidad; 
+
+	
+	
+}
